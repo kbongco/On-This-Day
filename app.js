b81305d0a6e065a828fcece98dff4856bf5155eb
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-26 13:05:16 -0400

    added data and DOM elements

--- app.js
+++ app.js
@@ -4,4 +4,35 @@ todaysDate = () => {
 
 todaysDate()
 
-eventURL = 'http://history.muffinlabs.com/date'
+const randomFact = (facts) => {
+  facts.forEach((fact) => {
+    let newDiv = document.createElement('div')
+    let year = document.createElement('h4')
+    year.innerText = year.year 
+    newDiv.classList.add('todaysevents')
+    newDiv.appendChild(year)
+    let description = document.createElement('p')
+    
+  })
+}
+
+eventURL = 'https://cors-anywhere.herokuapp.com/http://history.muffinlabs.com/date'
+
+
+const todayEvent = async () => {
+  const todaysEvent = eventURL
+  try {
+    const response = await axios.get(eventURL)
+    console.log(response.data)
+    console.log(response.data.data.Events)
+  } catch (error) {
+    console.error(`You got an ${error}, which enraged your father and punished you severely `)
+    //oversimplified references haha 
+  }
+} 
+todayEvent();
+
+
+
+
+
