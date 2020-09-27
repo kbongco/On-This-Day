0b5427a71450d42e9858afa86a128783eb6b0892
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-27 17:28:32 -0400

    added js and fixed api stuff

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ todaysDate = () => {
 todaysDate()
 
 const randomFact = (facts) => {
+  console.log(facts)
   facts.forEach((fact) => {
+    console.log(fact)
+    console.log(fact.text)
     let newDiv = document.createElement('div')
     let year = document.createElement('h4')
-    year.innerText = year.year 
+    year.innerText = fact.year 
+    console.log(year)
     newDiv.classList.add('todaysevents')
     newDiv.appendChild(year)
     let description = document.createElement('p')
-    
+    description.innerText = fact.text
+    console.log(description)
+    newDiv.appendChild(description)
+    document.querySelector('.event').append(newDiv)
   })
 }
 
@@ -25,6 +32,7 @@ const todayEvent = async () => {
     const response = await axios.get(eventURL)
     console.log(response.data)
     console.log(response.data.data.Events)
+    randomFact(response.data.data.Events)
   } catch (error) {
     console.error(`You got an ${error}, which enraged your father and punished you severely `)
     //oversimplified references haha 
